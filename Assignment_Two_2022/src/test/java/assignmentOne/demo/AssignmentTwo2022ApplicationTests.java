9627487ee12cebbe1e4932a8e53a908840a39f1e
Miguel Castanheira <7miguelc@gmail.com>
2022-03-25 00:11:26 +0000

    Second Commit

---
+++ Assignment_Two_2022/src/test/java/assignmentOne/demo/AssignmentTwo2022ApplicationTests.java
@@ -0,0 +1,18 @@
+package assignmentOne.demo;
+
+ 
+
+ 
+
+import assignmentOne.demo.AssignmentTwo2022ApplicationTests;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AssignmentTwo2022ApplicationTests.class)
+class AssignmentTwo2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
